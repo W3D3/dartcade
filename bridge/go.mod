57213a6330037099d3e54d562babea38dccbb2db
Christoph Wedenig <christoph@wedenig.org>
2026-09-24 19:58:20 +0200

    feat(bridge): scaffold module and generate schema types from adbridge-v1.json

---
+++ bridge/go.mod
@@ -0,0 +1,5 @@
+module dartcade/bridge
+
+go 1.22
+
+require github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
---
+++ bridge/internal/schema/generate.go
@@ -0,0 +1,3 @@
+package schema
+
+//go:generate go run github.com/atombender/go-jsonschema@latest --package schema --output schema_gen.go ../../../schema/adbridge-v1.json
